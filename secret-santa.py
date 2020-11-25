cf1abb7d4829a6c81de1ebe6e6680c4446682fa2
Bryan Deagle <bryan.deagle@ni.com>
2020-11-25 16:06:44 -0600

    got script working

--- secret-santa.py
+++ secret-santa.py
@@ -1,5 +1,7 @@
-from random import shuffle
+from jinja2 import Template
+import smtplib
 import hashlib
+import random
 import yaml
 
 SETTINGS_LOCATION = 'settings.yml'
@@ -15,9 +17,27 @@ with open('guests.yml', 'rt') as f:
 for guest in guests:  # Create hashes for each guest
     guest['hash'] = hashlib.sha1(guest['email'].encode()).hexdigest()
 
-shuffle(guests)  # Shuffle guest list and pair guests up
+random.shuffle(guests)  # Shuffle guest list and pair guests up
 pairs = [(f, t) for f, t in zip(guests[::2], guests[1::2])]
 
-# Write hashes to a YAML file for potential debug
+# Write hashes to a YAML file for future debug
 with open(settings['pairs-file'], 'wt') as f:
     yaml.dump([{'from': a['hash'], 'to': b['hash']} for (a, b) in pairs], f)
+
+# Create connection to SMTP Server
+server = smtplib.SMTP_SSL(settings['server'], 465)
+server.login(settings['username'], settings['password'])
+
+# Load body template from settings file
+template = Template(settings['body'])
+
+# Loop through pairs and send emails
+for from_guest, to_guest in pairs:
+    first = from_guest['name'].split(' ')[0]
+    body = template.render(first=first,
+                           name=to_guest['name'],
+                           address=to_guest['address'])
+    message = 'Subject: {}\n\n{}'.format(settings['subject'], body)
+    server.sendmail('santa@mg.dea.gl', from_guest['email'], message)
+
+server.quit()
